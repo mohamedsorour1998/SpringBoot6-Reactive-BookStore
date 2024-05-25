af480f713e358f7980bb052e45bc0809d7db757e
moahmedsorour1998 <mohamedsorour1998@gmail.com>
2024-05-25 16:33:27 +0300

    added the spring actuator.

--- src/main/java/app/rosettacloud/spring6reactive/config/SecurityConfig.java
+++ src/main/java/app/rosettacloud/spring6reactive/config/SecurityConfig.java
@@ -1,7 +1,8 @@
 package app.rosettacloud.spring6reactive.config;
-
+import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -9,8 +10,18 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
-    
     @Bean
+    @Order(1)
+    public SecurityWebFilterChain actuatorSecurityFilterChain(ServerHttpSecurity http) throws Exception {
+        http.securityMatcher(EndpointRequest.toAnyEndpoint())
+                .authorizeExchange(authorize -> authorize.anyExchange().permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
+
     SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http){
         http.authorizeExchange()
                 .anyExchange().authenticated()
