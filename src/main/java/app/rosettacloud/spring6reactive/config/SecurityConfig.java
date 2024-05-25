01f71978a96d2509ee03354b40c142e6a71489b8
moahmedsorour1998 <mohamedsorour1998@gmail.com>
2024-05-25 17:30:08 +0300

    fixed an issue in the actuator

--- src/main/java/app/rosettacloud/spring6reactive/config/SecurityConfig.java
+++ src/main/java/app/rosettacloud/spring6reactive/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package app.rosettacloud.spring6reactive.config;
+
 import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -10,6 +12,7 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
     @Bean
     @Order(1)
     public SecurityWebFilterChain actuatorSecurityFilterChain(ServerHttpSecurity http) throws Exception {
@@ -21,12 +24,11 @@ public class SecurityConfig {
 
     @Bean
     @Order(2)
-
-    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http){
-        http.authorizeExchange()
-                .anyExchange().authenticated()
-                .and()
-                .oauth2ResourceServer().jwt();
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
+                .oauth2ResourceServer(
+                        oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()))
+                .csrf(ServerHttpSecurity.CsrfSpec::disable);
 
         return http.build();
     }
